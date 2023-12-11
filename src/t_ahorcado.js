cd47cf628569d0d09b09da0302159b22de6240d3
Luchoragusa <luchito.rc4@gmail.com>
2023-12-11 18:26:11 -0300

    fixes

--- src/t_ahorcado.js
+++ src/t_ahorcado.js
@@ -1,6 +1,3 @@
-
-
-
 // create a list of easy work between 5 and 10 letters 
 export const palabrasFaciles = ['cara', 'leo', 'mama', 'papa', 'casa', 'perro', 'gato', 'pato', 'facil', 'ivo']
 // create a list of medium work between 10 and 15 letters
@@ -8,39 +5,23 @@ export const palabrasMedios = ["fantasma", "sandía", "escuela", "montaña", "gu
 // create a list of hard work between 15 and 20 letters
 export const palabrasDificiles = ['Extravagante', 'Inconformes', 'Desconcertado', 'Contradicción', 'Perseverante', 'Establecimiento', 'Espléndidamente', 'Anticonstitucional', 'Desesperadamente', 'Incomunicación'];
 
-
-
-
-
-
-
 export class Ahorcado {
-    // PALABRA_A_ADIVINAR = 'javascript'
-    // palabraUsuario = []
     MAX_INTENTOS = 6
-    // intentos = MAX_INTENTOS
     // TODO :: implemenmtar timer para que se termine el juego
     TIMER = 60
 
-    // create a constructor with the parameters
-    constructor(difucultad, val = 1) {
-        // this.palabraAdivinar = palabraAdivinar
-        // this.palabraUsuario = palabraUsuario
+    constructor(difucultad, palabraAdivinar = null) {
+        if (palabraAdivinar) {
+            this.palabraAdivinar = palabraAdivinar
+        } else {
+            if (difucultad === 'facil') this.palabraAdivinar = palabrasFaciles[Math.floor(Math.random() * palabrasFaciles.length)]
+            if (difucultad === 'medio') this.palabraAdivinar = palabrasMedios[Math.floor(Math.random() * palabrasMedios.length)]
+            else if (difucultad === 'dificil') this.palabraAdivinar = palabrasDificiles[Math.floor(Math.random() * palabrasDificiles.length)]
+        }
         this.difucultad = difucultad
         this.intentos = this.MAX_INTENTOS
         this.palabraUsuario = []
         this.letrasErradas = []
-        if (val === 2) {
-            if (difucultad === 'facil') this.palabraAdivinar = palabrasFaciles[Math.floor(Math.random() * palabrasFaciles.length)]
-            else if (difucultad === 'medio') this.palabraAdivinar = palabrasMedios[Math.floor(Math.random() * palabrasMedios.length)]
-            else if (difucultad === 'dificil') this.palabraAdivinar = palabrasDificiles[Math.floor(Math.random() * palabrasDificiles.length)]
-        } else {
-
-            if (difucultad === 'facil') this.palabraAdivinar = 'java'
-            else if (difucultad === 'medio') this.palabraAdivinar = 'python'
-            else if (difucultad === 'dificil') this.palabraAdivinar = 'javascript'
-        }
-
     }
 
     verificarLetra = (letra) => {
@@ -79,5 +60,4 @@ export class Ahorcado {
         this.letrasErradas = []
         this.intentos = this.MAX_INTENTOS
     }
-
 }
--- src/tests/ahorcado.test.js
+++ src/tests/ahorcado.test.js
@@ -1,85 +1,81 @@
 import { expect, test } from 'vitest'
 import { Ahorcado, palabrasFaciles, palabrasMedios, palabrasDificiles } from '../t_ahorcado'
 
-// // const juegoFacil = new Ahorcado('facil')
-// const juegoMedio = new Ahorcado('medio')
-// const juegoDificil = new Ahorcado('dificil')
-
 test('Verificar si una letra esta en la palabra', () => {
-    const juegoFacil = new Ahorcado('facil')
-    // pass the word throuth the constructor -- best practice
-    // ! TODO
+    const juegoFacil = new Ahorcado('facil', 'java')
+
     expect(juegoFacil.verificarLetra('a')).toBeTruthy();
 });
 test('Verificar si una letra no esta en la palabra', () => {
-    const juegoFacil = new Ahorcado('facil')
+    const juegoFacil = new Ahorcado('facil', 'java')
+    
     expect(juegoFacil.verificarLetra('x')).toBeFalsy();
 });
 
 test('Verificar si pasa una sola letra', () => {
-    const juegoFacil = new Ahorcado('facil')
+    const juegoFacil = new Ahorcado('facil', 'java')
 
     expect(juegoFacil.verificarLetra('xs')).toBeFalsy()
 });
 
 test('Verificar los intentos', () => {
-    const juegoFacil = new Ahorcado('facil')
+    const juegoFacil = new Ahorcado('facil', 'java')
 
     expect(juegoFacil.actualizarIntentos()).toBe(juegoFacil.intentos)
 });
 test('Verificar los intentos en cero', () => {
-    const juegoFacil = new Ahorcado('facil')
+    const juegoFacil = new Ahorcado('facil', 'java')
 
     expect(juegoFacil.actualizarIntentos()).toBe(juegoFacil.intentos)
 });
 test('Completar una letra ', () => {
-    const juegoFacil = new Ahorcado('facil')
+    const juegoFacil = new Ahorcado('facil', 'java')
 
     expect(juegoFacil.completarPalabra('j')).toStrictEqual(['j'])
 });
 test('Completar varias letras ', () => {
-    const juegoFacil = new Ahorcado('facil')
+    const juegoFacil = new Ahorcado('facil', 'java')
     // eslint-disable-next-line no-sparse-arrays
     expect(juegoFacil.completarPalabra('a')).toStrictEqual([, 'a', , 'a'])
 });
 test('No completar letra', () => {
-    const juegoFacil = new Ahorcado('facil')
+    const juegoFacil = new Ahorcado('facil', 'java')
 
     expect(juegoFacil.completarPalabra('q')).toStrictEqual([])
 });
 
 test('Validar si la letra erronea se agrego ', () => {
-    const juegoFacil = new Ahorcado('facil')
+    const juegoFacil = new Ahorcado('facil', 'java')
 
     expect(juegoFacil.agregarLetrasErradas('q')).toStrictEqual(['q'])
 });
 
 test('Validar si la letra correcta no se agrega ', () => {
-    const juegoFacil = new Ahorcado('facil')
+    const juegoFacil = new Ahorcado('facil', 'java')
 
     expect(juegoFacil.agregarLetrasErradas('v')).toBeFalsy()
 });
 
 test('Validar si la letra erronea se agrego a la lista previa ', () => {
-    const juegoFacil = new Ahorcado('facil')
+    const juegoFacil = new Ahorcado('facil', 'java')
     juegoFacil.letrasErradas = ['o']
     expect(juegoFacil.agregarLetrasErradas('q')).toStrictEqual(['o', 'q'])
 });
 
 test('Fin de juego con ganador', () => {
-    const juegoFacil = new Ahorcado('facil')
+    const juegoFacil = new Ahorcado('facil', 'java')
     juegoFacil.palabraUsuario = ['j', 'a', 'v', 'a']
     juegoFacil.intentos = 4
     expect(juegoFacil.verificarFinJuego()).toStrictEqual('Ganaste')
 });
 test('Fin de juego sin ganador', () => {
-    const juegoFacil = new Ahorcado('facil')
+    const juegoFacil = new Ahorcado('facil', 'java')
     juegoFacil.palabraUsuario = ['j', 'a']
     juegoFacil.intentos = 0
     expect(juegoFacil.verificarFinJuego()).toStrictEqual('Perdiste')
 });
 test('Reiniciar juego ', () => {
-    const juegoFacil = new Ahorcado('facil')
+    const juegoFacil = new Ahorcado('facil', 'java')
     juegoFacil.palabraUsuario = ['j', 'a']
     juegoFacil.intentos = 2
     juegoFacil.completarPalabra('j')
@@ -89,36 +85,34 @@ test('Reiniciar juego ', () => {
 });
 
 
-// test de otra dificultad
+// Tests de otra dificultad
 
 test('Validar juego medio', () => {
-    const juegofacil = new Ahorcado('facil')
+    const juegofacil = new Ahorcado('facil', 'java')
     expect(juegofacil.palabraAdivinar).toStrictEqual('java')
 });
 test('Validar juego medio', () => {
-    const juegoMedio = new Ahorcado('medio')
-
-    expect(juegoMedio.palabraAdivinar).toStrictEqual('python')
+    const juegoMedio = new Ahorcado('medio', 'guitarra')
+    expect(juegoMedio.palabraAdivinar).toStrictEqual('guitarra')
 });
 test('Validar juego dificil', () => {
-    const juegodificil = new Ahorcado('dificil')
-
-    expect(juegodificil.palabraAdivinar).toStrictEqual('javascript')
+    const juegodificil = new Ahorcado('dificil', 'Extravagante')
+    expect(juegodificil.palabraAdivinar).toStrictEqual('Extravagante')
 });
 
-// test con palabras al azar 
+// Tests con palabras al azar 
 
 test('Validar si la palabra elegida esta en la lista de las palabras faciles', () => {
-    const juegofacil = new Ahorcado('facil', 2)
+    const juegofacil = new Ahorcado('facil')
 
     expect(palabrasFaciles).toContain(juegofacil.palabraAdivinar)
 });
 test('Validar si la palabra elegida esta en la lista de las palabras medios', () => {
-    const juegoMedio = new Ahorcado('medio', 2)
+    const juegoMedio = new Ahorcado('medio')
 
     expect(palabrasMedios).toContain(juegoMedio.palabraAdivinar)
 });
 test('Validar si la palabra elegida esta en la lista de las palabras dificiles', () => {
-    const juegodificil = new Ahorcado('dificil', 2)
+    const juegodificil = new Ahorcado('dificil')
     expect(palabrasDificiles).toContain(juegodificil.palabraAdivinar)
 });
